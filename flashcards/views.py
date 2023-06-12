d7c661ed3344b695de5e4fe0ac224473fea91efc
Antonio <a.f.lupu@student.tudelft.nl>
2023-06-12 02:33:09 +0200

    Base Template + Started Landing Page
    I made the base template with django, and I started working with some Bootstrap classes for the initial landing page css.

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+cards = [
+    {
+        'front': 'Front 1',
+        'back': 'Back 1'
+    },
+    {
+        'front': 'Front 2',
+        'back': 'Back 2'
+    },
+    {
+        'front': 'Front 3',
+        'back': 'Back 3'
+    },
+    {
+        'front': 'Front 4',
+        'back': 'Back 4'
+    }
+]
+
 def home(request):
-    return render(request, 'flashcards/home.html')
+    return render(request, 'flashcards/home.html', {'title': 'Home'})
 
 
 def overview(request):
-    return render(request, 'flashcards/overview.html')
+    context = {
+        'cards': cards
+    }
+    return render(request, 'flashcards/overview.html', context)
