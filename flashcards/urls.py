563b0db53c40052f4887f87dbc1173f909251821
Antonio <a.f.lupu@student.tudelft.nl>
2023-07-13 17:55:30 +0200

    User input to CSV simple
    User inputs vocabulary list, webapp makes a csv file and downloads it to the user's machine

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='landing-page'),
     path('overview/', views.overview, name='overview-page'),
-    path('starting/', views.starting, name='starting-page')
+    path('starting/', views.starting, name='starting-page'),
+    path('generate-csv/', views.generate_csv, name='generate-csv')
 ]
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,3 +1,4 @@
+import csv
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -32,3 +33,27 @@ def overview(request):
 
 def starting(request):
     return render(request, 'flashcards/starting.html')
+
+def generate_csv(request):
+    # Get vocabulary data from textarea
+    vocabulary_data = request.POST.get('vocabulary_data')
+
+    # Create a CSV response object
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="vocabulary.csv"'
+
+    # Create a CSV writer
+    writer = csv.writer(response)
+
+    # Process each line of vocabulary data
+    lines = vocabulary_data.split('\n')
+    for line in lines:
+        line = line.strip()
+        if line:
+            # Split line into word and translation
+            word, translation = map(str.strip, line.split('='))
+
+            # write the word and translation to the CSV file
+            writer.writerow([word, translation])
+
+    return response
